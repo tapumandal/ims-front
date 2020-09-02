512ae29ee2c03a0c339e2d03a72d765fc8a60ae9
Tapu Mandal <online.tapu@gmail.com>
2020-09-03 04:55:19 +0600

    Login view added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './authentication/login/login.component';
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "login", component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
