bd0f30c3ee38c346ccf397a35dca5c9905906035
Tapu Mandal <online.tapu@gmail.com>
2020-09-05 10:12:33 +0600

    Chart appearing problem solve

--- src/app/business/dashboard/dashboard.component.ts
+++ src/app/business/dashboard/dashboard.component.ts
@@ -7,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+    title = 'Load script GFG';
+       constructor() {
+         this.loadScripts();
+       }
+
+       // Method to dynamically load JavaScript
+       loadScripts() {
+
+         // This array contains all the files/CDNs
+         const dynamicScripts = [
+            'assets/Chart.min.js',
+            'assets/dashboard3.js'
+         ];
+         for (let i = 0; i < dynamicScripts.length; i++) {
+           const node = document.createElement('script');
+           node.src = dynamicScripts[i];
+           node.type = 'text/javascript';
+           node.async = false;
+           document.getElementsByTagName('head')[0].appendChild(node);
+         }
+      }
 
   ngOnInit(): void {
   }
